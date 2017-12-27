c79534dbbe3628b99c237669417ae0099bb13f56
Zhi Wang <wanzhi@umich.edu>
2017-12-26 20:30:30 -0500

    added routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import { DatePicker } from 'antd';
-import 'antd/dist/antd.css'
-const React = require('react');
-const ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDom from 'react-dom';
+import {
+	BrowserRouter as Router,
+	Route,
+	Link
+} from 'react-router-dom';
 
-// render on page
-ReactDOM.render(
-  <div>
-    <h1>Hello World!</h1>
-    <DatePicker />
-  </div>,
-  document.getElementById('app')
+import Home from './home';
+import About from './about';
+import Topics from './topics';
+
+ReactDom.render(
+	<div>
+		<Router>
+			<div>
+				<ul>
+					<li><Link to='/'>Home</Link></li>
+					<li><Link to='/about'>About</Link></li>
+					<li><Link to='/topics'>Topics</Link></li>
+				</ul>
+				<hr/>
+				<Route exact path="/" component={Home}/>
+				<Route path="/about" component={About}/>
+				<Route path="/topics" component={Topics}/>
+			</div>
+		</Router>
+		<p>nihao!</p>
+	</div>
+	,
+	document.getElementById('app')
 );
