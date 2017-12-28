f03f93309c748ed48276e493a38465ea0562fc0a
Hua Chen <huachen@Huas-MacBook-Pro.local>
2017-12-28 18:11:24 -0500

    layout

--- src/order.js
+++ src/order.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Button, Row, Col } from 'antd';
-import 'antd/dist/antd.css';
 
-// const Table = props => <p className={`height-${props.value}`}>{props.childrem}</p>;
+// var ReactGridLayout = require('react-grid-layout');
+
+import ReactGridLayout from 'react-grid-layout';
+
+var layout = [
+  {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
+  {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+  {i: 'c', x: 4, y: 0, w: 1, h: 2}
+];
 
 
 export default () => (
 	<div>
-		<Row type='flex' justify='space-between' align = 'bottom'>
-			<Col span={100} push={4}>1</Col>
-			<Col span={4}><Button type='primary' size='large'>Table1</Button></Col>
-			<Col span={4}><Button type='primary' size='large'>Table1</Button></Col>
-			<Col span={4}><Button type='primary' size='large'>Table1</Button></Col>
-		</Row>
+		<ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+	        <div key="a">a</div>
+	        <div key="b">b</div>
+	        <div key="c">c</div>
+	    </ReactGridLayout>
 	</div>
 );
