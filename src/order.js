d2aad2da565f8ddb054d11ea9b6bba880995811b
Zhi Wang <wanzhi@umich.edu>
2017-12-28 20:06:38 -0500

    added table component

--- src/order.js
+++ src/order.js
@@ -1,28 +1,24 @@
 import React from 'react';
+import { Row, Col } from 'antd';
 
-// var ReactGridLayout = require('react-grid-layout');
-
-import ReactGridLayout from 'react-grid-layout';
-
-var layout = [
-  {i: 'a', x: 0, y: 0, w: 3, h: 2, static: true},
-  {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
-  {i: 'c', x: 4, y: 0, w: 3, h: 2}
-];
 
 var styles = {
 	width: 100,
 	height: 100,
-	color: "red",
-	backgroundColor: "black"
+	color: 'white',
+	backgroundColor: 'blue'
 };
 
 export default () => (
 	<div>
-		<ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200} verticalCompact={false}>
-	        <div key="a"><button style = {styles}> A </button></div>
-	        <div key="b"><button style = {styles}> B </button></div>
-	        <div key="c"><button style = {styles}> C </button></div>
-	    </ReactGridLayout>
+	<Row type="flex">
+		<button style= {styles}> test </button>
+	</Row>
+	<Row type="flex">
+		<button style= {styles}> test </button>
+	</Row>
+	<Row type="flex" justify="center">
+		<button style= {styles}> test </button>
+	</Row>
 	</div>
 );
---
+++ src/table.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Table extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			server_id: undefined,
+			order: [],
+			is_open: false
+		};
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	
+
+	render() {
+		return (
+
+		);
+	}
+}
